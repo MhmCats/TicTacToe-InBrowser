dfaf26aecfe40fef6c7cb07bbc8010290e1835e1
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-02-11 20:45:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+### Tic Tac Toe
+
+#### What it is.
+
+I made this so anyone could hit Ctrl-Shift-I or whatever the keyboard shortcut for the developer tools is open up the JavaScript console copy and paste the code in and play this with their friends (or just on your own if you uh do that kind of thing).
+
+#### How to play on console
+
+Navigate to the folder `console_version` and open either the file `pretty.js` or `compressed.js`. The compressed version is only on one line and is completely unreadable which is why I included a version that you can see the code used if you want to.
+
+#### Playing in an HTML file
+
+At the moment I have not created this but it is a feature that I hope to make soon.
+
+#### The controls
+
+The controls go as follows:
+
+A blank board looks like this:
+```
+-   -   -
+-   -   -
+-   -   -
+```
+If I were to input the number 1 then the board would update as follows:
+```
+x   -   -
+-   -   -
+-   -   -
+```
+If then the next player inputs the number 7 then:
+```
+x   -   -
+-   -   -
+o   -   -
+```
+They are ordered logically with the first row being:
+```
+-   -   -
+1   2   3
+```
+The 2nd row:
+```
+-   -   -
+4   5   6
+```
+And the 3rd row:
+```
+-   -   -
+7   8   9
+```
+
+#### Bugs
+
+If you find any bugs then make an issue to report it or a pull request if you think you have found a solution although I would rather that you tested out any solutions before you created a pull request.
+
+Thank you for playing or just stopping by to see it.
+
+#### To do list
+
+- [ ] Make an HTML version instead of just using prompts
+- [ ] Make something that handles when you input an invalid number or a number that has already been used.
---
+++ console_version/pretty.js
@@ -0,0 +1,85 @@
+(function () {
+    var player1 = "x";
+    var player2 = "o";
+    function createNewBoard() {
+        return [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+    };
+    function checkWin(board, player) {
+        for (let i = 0; i < 3; i++) {
+            if(board[0][i] === player && board[1][i] === player && board[2][i] === player){
+                return true;
+            };
+            if(board[i][0] === player && board[i][1] === player && board[i][2] === player){
+                return true;
+            };
+        };
+        if(board[0][0] === player && board[1][1] === player && board[2][2] === player){
+            return true;
+        };
+        if(board[2][0] === player && board[1][1] === player && board[0][2] === player){
+            return true;
+        } else{
+            return false
+        };
+    };
+    function checkDraw(board) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === "-"){
+                    return false;
+                };
+            };
+        };
+        if (checkWin(board, "x") === true || checkWin(board, "o") === true){
+            return false;
+        };
+        return true;
+    };
+    function playerMove(board, player, moveInt) {
+        if (parseInt(moveInt) < 4) {
+            if (board[0][parseInt(moveInt)-1] === "-") {
+                board[0][parseInt(moveInt)-1] = player;
+            };
+        } else if (parseInt(moveInt) < 7) {
+            if (board[1][parseInt(moveInt)-4] === "-"){
+                board[1][parseInt(moveInt)-4] = player;
+            };
+        } else if (parseInt(moveInt) < 10) {
+            if (board[2][parseInt(moveInt)-7] === "-") {
+                board[2][parseInt(moveInt)-7] = player;
+            } else {
+                return false;
+            };
+        } else {
+            return false;
+        };
+        return board;
+    };
+    function printableBoard(board) {
+        var res = "";
+        for (let row in board) {
+            var rowString = "";
+            for (let item in board[row]) {
+                rowString += `${board[row][item]}   `;
+            };
+            res += `${rowString}\n`;
+        };
+        return res;
+    };
+
+    var board = createNewBoard();
+    while (true) {
+        board = playerMove(board, player1, prompt(`${printableBoard(board)} Player 1 what is your move?`));
+        if (checkWin(board, player1)) {
+            return alert("Player 1 won!");
+        } else if (checkDraw(board)) {
+            return alert("It was a draw!");
+        };
+        board = playerMove(board, player2, prompt(`${printableBoard(board)} Player 2 what is your move?`));
+        if (checkWin(board, player2)) {
+            return alert("Player 2 won!");
+        } else if (checkDraw(board)) {
+            return alert("It was a draw!");
+        };
+    };
+})()
